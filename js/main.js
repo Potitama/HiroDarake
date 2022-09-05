e90bedc62e13a92ace7f20ad832eea7d5d62d1a7
Potitama <111692445+Potitama@users.noreply.github.com>
2022-09-05 19:44:30 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,3 +9,13 @@ $(window).scroll(function() {
         }
     });
 }); 
+
+jQuery(function ($) {
+    $('.js-accordion-title').on('click', function () {
+      /*クリックでコンテンツを開閉*/
+      $(this).next().slideToggle(200);
+      /*矢印の向きを変更*/
+      $(this).toggleClass('open', 200);
+    });
+    
+});
